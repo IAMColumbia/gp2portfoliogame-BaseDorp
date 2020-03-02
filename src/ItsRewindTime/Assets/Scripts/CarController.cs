57ba3ace8214c97f2aed0c98557d22971ad231ef
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2020-03-02 17:34:29 -0600

    added driveable car

---
+++ src/ItsRewindTime/Assets/Scripts/CarController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InputManager))]
+public class CarController : MonoBehaviour
+{
+    public InputManager inputManager;
+    public List<WheelCollider> throttleWheels;
+    public List<WheelCollider> steeringWheels;
+    float strengthCoefficient = 20000f;
+    float maxTurn = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputManager = GetComponent<InputManager>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        foreach (WheelCollider wheel in throttleWheels)
+        {
+            wheel.motorTorque = strengthCoefficient * Time.deltaTime * inputManager.throttle;
+        }
+
+        foreach (WheelCollider wheel in steeringWheels)
+        {
+            wheel.steerAngle = maxTurn * inputManager.steer;
+        }
+    }
+}
---
+++ src/ItsRewindTime/Assets/Scripts/InputManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public float throttle;
+    public float reverse;
+    public float steer;
+    public bool breaking;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMovement();
+    }
+
+    void UpdateMovement()
+    {
+        throttle = Input.GetAxis("Forward");
+        reverse = Input.GetAxis("Reverse");
+        steer = Input.GetAxis("Horizontal");
+
+        throttle = throttle - reverse;
+
+        if (reverse > 0)
+        {
+            breaking = true;
+        }
+        else
+        {
+            breaking = false;
+        }
+    }
+}
